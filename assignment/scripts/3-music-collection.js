44eec0ff0e2686601c1d6890a831cfb04f3d74a8
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-02-16 13:46:03 -0600

    Added showCollection Function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -22,6 +22,11 @@ console.log('Adding The Chemical Brothers, Dig Your Own Hole, 1997 to the collec
 console.log('Adding Roni Size, New Forms, 1997 to the collection:', addToCollection('Roni Size', 'New Forms', 1997));
 console.log(collection);
 
-function showCollection(show){
-
+function showCollection(array){
+  console.log(array.length);
+  for(i=0; i<array.length; i++){
+    console.log(array[i].title + ' by ' + array[i].artist + ' published in ' + array[i].yearPublished);
+    }
 }
+
+showCollection(collection);
