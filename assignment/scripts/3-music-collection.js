4433c07ee4b709a3096dd3abcefdda5f783a5701
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-02-16 13:36:07 -0600

    Added 6 Records to Collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,9 +4,9 @@ let collection = [];
 
 function addToCollection(title, artist, yearPublished){
   let record = {
-    title:,
-    artist:,
-    yearPublished:
+    title: 'title',
+    artist: 'artist',
+    yearPublished: 'yearPublished'
   }
 
   record.title = title;
@@ -14,4 +14,14 @@ function addToCollection(title, artist, yearPublished){
   record.yearPublished = yearPublished;
   collection.push(record);
   return record;
+}
+console.log('Adding Massive Attack, Blue Lines, 1991 to the collection:', addToCollection('Blue Lines', 'Massive Attack', 1991));
+console.log('Adding Massive Attack, Protection, 1995 to the collection:', addToCollection('Protection', 'Massive Attack', 1995));
+console.log('Adding Nine Inch Nails, The Downward Spiral, 1994 to the collection:', addToCollection('The Downward Spiral', 'Nine Inch Nails', 1994));
+console.log('Adding The Chemical Brothers, Dig Your Own Hole, 1997 to the collection:', addToCollection('Dig Your Own Hole', 'The Chemical Brothers', 1997));
+console.log('Adding Roni Size, New Forms, 1997 to the collection:', addToCollection('Roni Size', 'New Forms', 1997));
+console.log(collection);
+
+function showCollection(show){
+
 }
