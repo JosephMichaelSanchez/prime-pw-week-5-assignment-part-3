6c1e8af0e80491d632bb3d71e57180f61e819877
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-02-16 12:23:35 -0600

    Created addToCollection Function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,17 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+  let record = {
+    title:,
+    artist:,
+    yearPublished:
+  }
+
+  record.title = title;
+  record.artist = artist;
+  record.yearPublished = yearPublished;
+  collection.push(record);
+  return record;
+}
